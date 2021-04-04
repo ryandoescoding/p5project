345a83f3e7d5a043a0f6547810c9f72194055428
ryandoescoding <bluejinu1@gmail.com>
2021-04-04 16:21:27 +0900

    mkp

---
+++ sketch.js
@@ -0,0 +1,40 @@
+let bf_url = "https://coolors.co/f72585-b5179e-7209b7-560bad-480ca8-3a0ca3-3f37c9-4361ee-4895ef-4cc9f0";
+bf_url = bf_url.split("/");
+let url = bf_url[3];
+//nsole.log(url);
+
+
+
+let color_cd = [];
+
+let new_color = bf_url[3].split("-");
+
+let result_color =  "";
+                            
+//conle.log(new_color.length);
+
+for(let i = 0; i < new_color.length; i++){
+  //nsole.log(colors[0]);
+   color_cd[i] = "#" + new_color[i];
+
+  
+}
+
+console.log(color_cd);
+
+function setup() {
+  createCanvas(1000, 1000);
+  
+  
+}
+
+function draw() {
+  background(220);
+  
+  
+    
+  let c = random(color_cd);
+  fill(c);
+  ellipse(100,100,100,100);
+
+}
